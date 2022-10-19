7c41e77c05befcf4aefacb7acd06a7229b0e355a
Jddelara01 <jddelara01@gmail.com>
2022-10-19 18:49:59 +0000

    Add the menu content and function to open menu tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,43 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
   });
+
+
+  /**
+   * To open the menu that the user clicked 
+   */
+  function openMenu(tabName, element) {
+    // Hide all elements with class name "menuTab" by default 
+    let tabContent;
+    tabContent = document.getElementsByClassName("menuTab");
+    for(let i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+
+    // Remove the background color and text decoration of all tabs
+    let tabLinks;
+    tabLinks = document.getElementsByClassName("tablink");
+    for(let i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].style.backgroundColor = "";
+      tabLinks[i].style.textDecoration = "none";
+    }
+
+
+    // Display the specific clicked tab content
+    document.getElementById(tabName).style.display = "block";
+
+    // Add background color and underline the specific clicked tab
+    element.style.backgroundColor = "rgba(238, 164, 127, 0.8)";
+    element.style.textDecoration = "underline";
+
+  }
+
+  // Display the element with id "defaultTab" when opening the menu page
+  document.getElementById("defaultTab").click();
+
+/**
+ * Display the menu options based on the tab clicked
+ */
+function displayMenu(menuType) {
+  
+}
